cfde87e0cc3e3a828bc244d80a95d17188dedad7
MikHM <hemroda.martin@gmail.com>
2017-02-24 13:31:22 +0100

    Service for counting the tickets availability added.

---
+++ src/Louvre/BookingBundle/Services/CheckTicketsAvailability.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Louvre\BookingBundle\Services;
+
+
+use Doctrine\ORM\EntityManager;
+
+class CheckTicketsAvailability
+{
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+
+    public function selectedDayTickes($dayToBeChecked)
+    {
+        // fetches the BookingRepository custom method
+        $bookings = $this->em->getRepository("BookingBundle:Booking")->getNumberOfBookingsPerDay($dayToBeChecked);
+
+        // will be used for total of tickets
+        $ticketsPerBooking = 0;
+
+        foreach ($bookings as $booking)
+        {
+            $ticketsPerBooking += $booking->getNumberOfBookingsPerDay();
+        }
+
+        return $ticketsPerBooking;
+        
+    }
+
+}
